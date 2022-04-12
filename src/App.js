fc444812bc0a9456f5120946b3077fb5d833a7c5
CMTSanein <constantinemichaelt@gmail.com>
2022-04-12 15:54:32 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./TextArea";
 import TextArea from "./TextArea";
 import "./TextArea.css";
 import Button from "./Button";
+import NoteList from "./NoteList";
 
 function App() {
 	return (
 		<div className="App">
+			<NoteList />
 			<TextArea />
 			<Button />
 		</div>
---
+++ src/NoteList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NoteList.css";
+import Note from "./Note";
+
+const NoteList = () => {
+	return (
+		<div className="NoteList">
+			<Note />
+		</div>
+	);
+};
+
+export default NoteList;
